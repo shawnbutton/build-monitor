8b9f498fb2091fe598c5ec3fef70a08e40ed8a85
shawn <shawnbutton@gmail.com>
2024-07-28 11:23:40 -0400

    add theme changer for dark mode

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Sun from "./Sun.svelte";
+	import { THEMES } from "$lib/config";
+
+	function set_theme(theme: string) {
+		if (!Object.values(THEMES).includes(theme)) return;
+		localStorage.setItem("theme", theme);
+		document.body.setAttribute("data-theme", theme);
+	}
+
+	function toggle_theme(): void {
+		let current_theme = document.body.getAttribute("data-theme");
+		const theme =
+			current_theme === THEMES.LIGHT
+				? THEMES.DARK
+				: THEMES.LIGHT;
+		set_theme(theme);
+	}
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost text-xl">Monte Carlo Forecaster</a>
+	</div>
+	<div class="flex-none">
+		<button aria-label="toggle theme" on:click={toggle_theme}>
+			<Sun />
+		</button>
+	</div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import 'tailwindcss/tailwind.css';
+	import Nav from "$lib/components/Nav.svelte";
+</script>
+
+
+<div class="pl-10 pr-10">
+	<Nav />
+	<slot />
+</div>
+
